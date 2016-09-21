12c20994633bf8852f05dd81d5b86929610dbf59
Sean Dow Miller <seandowmiller@gmail.com>
2016-09-21 11:31:00 -0700

    getting closer to getting the robot to create response
    almost(?) response

--- chatbot.js
+++ chatbot.js
@@ -3,7 +3,7 @@
 var inputTalk
 var question
 
-
+var answer
 
 
 
@@ -13,9 +13,14 @@ var question
 function talkInput(){
 inputTalk = document.getElementById("input").value;
 //document.getElemmentById("chat-area").textContent += inputTalk;
-question = "Lorenzo: " + inputTalk + "<br>";
+question = "LeSean James: " + inputTalk + "<br>";
 document.getElementById("chat-area").innerHTML += question;
 
+var n = question.search(/how are you/i);
+console.log(n);
 
 inputTalk = document.getElementById("input").value = null;
+
+if(n > -1){
+  answer = "Martin Luther: I am well";
 }
