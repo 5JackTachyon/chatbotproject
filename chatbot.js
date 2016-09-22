326d411bc7d2ae99ac3970f78146932261e40904
Sean Dow Miller <seandowmiller@gmail.com>
2016-09-22 11:31:15 -0700

    added another response
    progress!

--- chatbot.js
+++ chatbot.js
@@ -18,12 +18,17 @@ document.getElementById("chat-area").innerHTML += question;
 
 
 var n = question.search(/how are you/i);
+var o = question.search(/how old are you/i);
 console.log(n);
 
 inputTalk = document.getElementById("input").value = null;
 
 
 if(n > -1){
-  document.getElementById("chat-area").innerHTML += "Martin Luther: I am well, I am glad to see the direction that society has gone " + "<br>";
+  document.getElementById("chat-area").innerHTML += "Martin Luther: I am well, I am glad to see the direction that society has gone. " + "<br>";
+}
+
+if(o > -1){
+  document.getElementById("chat-area").innerHTML += "Martin Luther: I am a little past my prime. " + "<br>";
 }
 }
