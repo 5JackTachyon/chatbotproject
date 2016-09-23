ee6426248f378c54d1eb839f2e0fe95d800c126f
Sean Dow Miller <seandowmiller@gmail.com>
2016-09-23 11:19:57 -0700

    got time!!

--- chatbot.js
+++ chatbot.js
@@ -18,7 +18,12 @@ document.getElementById("chat-area").innerHTML += question;
 
 
 var n = question.search(/how are you/i);
-var o = question.search(/how old are you/i);
+var o = question.search(/old/i);
+var t = question.search(/time/i);
+var u = question.search(/year/i);
+var v = question.search(/day/i);
+var w = question.search(/date/i);
+var da = new Date(Date.now());
 console.log(n);
 
 inputTalk = document.getElementById("input").value = null;
@@ -30,5 +35,18 @@ if(n > -1){
 
 if(o > -1){
   document.getElementById("chat-area").innerHTML += "Martin Luther: I am a little past my prime. " + "<br>";
+}
+
+if(t > -1){
+document.getElementById("chat-area").innerHTML += "Martin Luther: The time is " + da + "." + "<br>";
+}
+if(u > -1){
+  document.getElementById("chat-area").innerHTML += "Martin Luther: According to my primary sources, the year is " + da.getFullYear() + "." + "<br>";
+}
+if(v > -1){
+  document.getElementById("chat-area").innerHTML += "Martin Luther: Today is the " + da.getDay() + "th day of the week not that that matters much." + "<br>";
+}
+if(w > -1){
+  document.getElementById("chat-area").innerHTML += "Martin Luther: The date is " + da.getDate() + " is that that the type of date you meant?" + "<br>";
 }
 }
